a0252e7c725c68a752290e69c02ae52ed50d6163
kon28289 <kon28289@naver.com>
2024-12-13 16:23:58 +0900

    백엔드임

---
+++ src/main/kotlin/kr/ac/kumoh/s20210080/w24BackEnd/W24BackEndApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210080.w24BackEnd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24BackEndApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24BackEndApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210080/w24BackEnd/W24BackEndApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210080.w24BackEnd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24BackEndApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
